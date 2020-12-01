6a11fff93bf739278326145c1c5f78be2f626824
Milad Irannejad <milad@uber.com>
2020-12-01 01:09:20 -0500

    update-formulas: iterate over formula files

--- .github/actions/update-formulas/src/update.js
+++ .github/actions/update-formulas/src/update.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+
 const core = require('@actions/core')
-const github = require('@actions/github')
+
+const regex = /url\s+"(https:\/\/github.com\/moorara\/[0-9A-Za-z._-]+)",\s+tag:\s+"(v?[0-9]+\.[0-9]+\.[0-9]+)",\s+revision:\s+"([0-9a-f]{40})"/g
 
 async function run () {
   try {
@@ -9,8 +12,22 @@ async function run () {
     // Set output variables
     // core.setOutput('output', 'value')
 
-    // Testing
-    core.info(JSON.stringify(github.context, null, 2))
+    // Iterate over all files in the current directory to find *.rb files
+    const files = await fs.promises.readdir('.')
+    for (const file of files) {
+      if (file.endsWith('.rb')) {
+        const [formula] = file.split('.').slice(0, -1).join('.')
+        core.info(`Formula ${formula} found`)
+
+        const content = await fs.promises.readFile(file)
+        const matches = [...content.matchAll(regex)]
+        const [, url, tag, revision] = matches[0]
+
+        core.info(`url: ${url}`)
+        core.info(`tag: ${tag}`)
+        core.info(`revision: ${revision}`)
+      }
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
